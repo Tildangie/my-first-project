d790417a241762e7a9128b54485c0f0280be6d73
Tildangie <angieland55@gmail.com>
2024-03-07 13:34:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var total = 11;
+console.log(total)
+
+let a = 3+6
+console.log(a)
+let c= 2;
+let d= 4;
+let e= c+d;
+console.log(e);
+
+let r= 2;
+let j= 4;
+let h= r*j;
+console.log(h);
+
+let first = "coding" ;
+let second = " is fun";
+let result = first + second;
+console.log(result);
